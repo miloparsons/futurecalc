c603045acc8ae35d0a4f6df5932114c3e9831c8e
mparsons <mparsons@box.com>
2021-05-10 20:17:24 +0100

    Deploy

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,16 @@
     var entry = document.getElementById('entry_price').value;
     var leverage = document.getElementById('leverage').value;
     var roe = document.getElementById('desired_roe').value;
-    var val_1 = (roe/100) * leverage;
+    var val_1 = (roe/100) / leverage;
     var val_2 = val_1 + 1;
-    var val_3 = val_2 * entry;
+    var val_3 = Number(Math.round((val_2 * entry) +'e5')+'e-5');
     document.getElementById('exit_at').innerHTML = val_3;
+
+    console.log(`Entry = ${entry}`);
+    console.log(`Leverage = ${leverage}`);
+    console.log(`ROE = ${roe}`);
+    console.log(`Val 1 = ${val_1}`);
+    console.log(`Val 2 = ${val_2}`);
+    console.log(`Val 3 = ${val_3}`);
+    return false;
 }
